88d2ae09714a51585b3a2b5da5c28fdf7268bbeb
amartin <alberto.martin@monash.edu>
2020-10-27 23:33:32 +1100

    Removed mpirun's OpenMPI tailored flags

--- test/runtests.jl
+++ test/runtests.jl
@@ -29,14 +29,14 @@ using PETSc
 
 nprocs_str = get(ENV, "JULIA_GRIDAPDISTRIBUTED_TEST_NPROCS","")
 nprocs = nprocs_str == "" ? clamp(Sys.CPU_THREADS, 2, 4) : parse(Int, nprocs_str)
-mpiexec_args = Base.shell_split("--allow-run-as-root --tag-output") #Base.shell_split(get(ENV, "JULIA_MPIEXEC_TEST_ARGS", ""))
+#mpiexec_args = Base.shell_split("--allow-run-as-root --tag-output") #Base.shell_split(get(ENV, "JULIA_MPIEXEC_TEST_ARGS", ""))
 testdir = @__DIR__
 istest(f) = endswith(f, ".jl") && startswith(f, "MPI")
 testfiles = sort(filter(istest, readdir(testdir)))
 @testset "$f" for f in testfiles
   MPI.mpiexec() do cmd
      println("$(f)")
-     cmd = `$cmd $mpiexec_args`
+     #cmd = `$cmd $mpiexec_args`
      np = nprocs
      if f in ["MPIPETScDistributedVectorsTests.jl","MPIPETScDistributedIndexSetsTests.jl"]
        np = 2
