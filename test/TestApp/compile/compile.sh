b8e9c72d9f4fd93e97b4a38b823bdb1d7b45644c
Francesc Verdugo <fverdugo@cimne.upc.edu>
2021-11-06 08:59:26 +0100

    Fix Pkg warning

--- test/TestApp/compile/compile.sh
+++ test/TestApp/compile/compile.sh
@@ -3,5 +3,5 @@
 # This script is to be executed from this folder (compile/)
 # See https://juliaparallel.github.io/MPI.jl/latest/knownissues/#Julia-module-precompilation-1
 # for a justification of this line
-julia --project=../ --color=yes -e 'using Pkg; pkg"precompile"'
+julia --project=../ --color=yes -e 'using Pkg; Pkg.precompile()'
 julia --project=../ -O3 --check-bounds=no --color=yes compile.jl $1
