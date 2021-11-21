049f80f489d4c7ec83d4dda9dbe18bf4f1495cfd
amartin <alberto.martin@monash.edu>
2021-11-21 23:39:28 +1100

    Added a test_broken to test/sequential/runtests.jl

--- test/sequential/runtests.jl
+++ test/sequential/runtests.jl
@@ -1,6 +1,8 @@
 module SequentialTests
 
 using Test
+using PartitionedArrays
+using Gridap
 
 @time @testset "Geometry" begin include("GeometryTests.jl") end
 
@@ -13,5 +15,16 @@ using Test
 @time @testset "Poisson" begin include("PoissonTests.jl") end
 
 @time @testset "PLaplacian" begin include("PLaplacianTests.jl") end
+
+parts = get_part_ids(sequential,(1,1))
+ids   = PRange(parts,6)
+vec   = PVector(1.0,ids);
+ids2  = PRange(parts,12)
+vec2  = PVector(2.0,ids);
+vec3vals = map_parts(vec2.owned_values) do ov
+  Gridap.Arrays.SubVector(ov,1,6)
+end
+vec3  = PVector(vec3vals,ids);
+@test_broken vec .= vec .+ vec3
 
 end # module
