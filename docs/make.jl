6cd733b8969f3a30cb9aeb2805ec4925e75e6ca2
amartin <alberto.martin@monash.edu>
2022-02-11 12:35:46 +1100

    Mods in author doc

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(;
     ],
     repo="https://github.com/gridap/GridapDistributed.jl/blob/{commit}{path}#L{line}",
     sitename="GridapDistributed.jl",
-    authors="Francesc Verdugo <fverdugo@cimne.upc.edu>",
+    authors="S. Badia <santiago.badia@monash.edu>", "A. F. Martin <alberto.martin@monash.edu>", "F. Verdugo <fverdugo@cimne.upc.edu>",
     assets=String[],
 )
 
