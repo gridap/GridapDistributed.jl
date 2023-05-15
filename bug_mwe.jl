14c60e5e960eb45337d2a79f4d94af5b6f26f8e8
JordiManyer <jordi.manyer@monash.edu>
2023-05-15 18:57:29 +1000

    Added MWE for bug

---
+++ bug_mwe.jl
@@ -0,0 +1,29 @@
+using Gridap
+using Gridap.Geometry
+using Gridap.Arrays
+using GridapDistributed
+using PartitionedArrays
+
+backend = SequentialBackend()
+ranks   = (1,2)
+parts   = get_part_ids(backend,ranks)
+
+domain    = (0.0,1.0,0.0,1.0)
+partition = (4,4)
+model     = CartesianDiscreteModel(parts,domain,partition)
+
+order = 1
+reffe = ReferenceFE(raviart_thomas,Float64,order)
+V = TestFESpace(model,reffe,conformity=:HDiv)
+U = TrialFESpace(V)
+
+qdegree = 2*order+1
+Ω   = Triangulation(model)
+dΩ  = Measure(Ω,qdegree)
+
+Λ   = SkeletonTriangulation(model)
+dΛ  = Measure(Λ,qdegree)
+n_Λ = get_normal_vector(Λ)
+
+biform(u,v) = ∫(u⋅v)*dΩ - ∫(jump(v⊗n_Λ)⊙(mean(∇(u))))dΛ
+A = assemble_matrix(biform,U,V)
