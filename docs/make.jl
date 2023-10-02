a5fb46e86cc3d69726d7462f9381f1cd693f2d7c
JordiManyer <jordi.manyer@monash.edu>
2023-10-02 15:01:08 +1100

    Minor

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ pages = [
   "GridapDistributed" => "GridapDistributed.md",
   "Algebra" => "Algebra.md",
   "CellData" => "CellData.md",
-  "DivConformingFESpaces" => "DivConformingFESpaces.md",
   "FESpaces" => "FESpaces.md",
   "Geometry" => "Geometry.md",
   "MultiField" => "MultiField.md",
@@ -21,6 +20,7 @@ makedocs(;
     repo="https://github.com/gridap/GridapDistributed.jl/blob/{commit}{path}#L{line}",
     sitename="GridapDistributed.jl",
     authors="S. Badia <santiago.badia@monash.edu>, A. F. Martin <alberto.f.martin@anu.edu.au>, F. Verdugo <fverdugo@cimne.upc.edu>",
+    # warnonly=true, # for debugging
 )
 
 deploydocs(;
