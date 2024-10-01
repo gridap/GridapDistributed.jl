a3309eef48b97bef3f9e5f3517a2716f86339b04
JordiManyer <jordi.manyer@monash.edu>
2024-10-01 10:12:33 +1000

    Minor

--- test/ConstantFESpacesTests.jl
+++ test/ConstantFESpacesTests.jl
@@ -2,19 +2,22 @@
 using Gridap
 using GridapDistributed, PartitionedArrays
 
-using Gridap.FESpaces
+using Gridap.FESpaces, Gridap.Arrays, Gridap.Algebra
 
 np = (2,2)
-ranks = with_debug() do distribute
+ranks = with_mpi() do distribute
   distribute(LinearIndices((prod(np),)))
 end
 
-model = CartesianDiscreteModel(ranks,np,(0,1,0,1),(4,4))
+model = CartesianDiscreteModel(ranks,np,(0,1,0,1),(10,10))
 
 Ω = Triangulation(model)
 dΩ = Measure(Ω,2)
 
+reffe = ReferenceFE(lagrangian, Float64, 1)
+U = FESpace(model,reffe)
 V = ConstantFESpace(model)
+X = MultiFieldFESpace([U,V])
 
 gids = get_free_dof_ids(V)
 local_to_global(gids)
@@ -25,3 +28,19 @@ own_to_local(gids)
 uh = zero(V)
 sum(∫(uh)dΩ)
 
+map(ranks) do r
+  println("flag 1 from ", r)
+end
+
+a((u,λ),(v,μ)) = ∫(u*v + λ*v + μ*u)dΩ 
+A = assemble_matrix(a,X,X)
+
+map(ranks) do r
+  println("flag 2 from ", r)
+end
+
+x = allocate_in_domain(A)
+
+map(ranks) do r
+  println("done from ", r)
+end
